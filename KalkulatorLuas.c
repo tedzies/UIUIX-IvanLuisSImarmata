7b74d9c8755bf0e3ded97cb20c1ef2b7ef3f1f7c
Luis <ivan_luis030602@protonmail.com>
2022-08-22 10:00:17 +0800

    Fitur Hitung Luas Persegi Panjang

--- KalkulatorLuas.c
+++ KalkulatorLuas.c
@@ -17,4 +17,13 @@ int main()
     hasil = nil1 * 2;
     printf("Luas Persegi adalah: %d.\n", hasil);
   }
+  if (pil == 2)
+  {
+    printf("Masukkan panjang persegi panjang: ");
+    scanf("%d", &nil1);
+    printf("Masukkan lebar persegi panjang: ");
+    scanf("%d", &nil2);
+    hasil = nil1 * nil2;
+    printf("Luas Persegi Panjang adalah: %d.\n", hasil);
+  }
 }
